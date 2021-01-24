0e4a07b1ad94ce2cf994e112dd5ea0154ef4dc17
Jeff Herb <herb.jeff@gmail.com>
2021-01-23 23:09:51 -0500

    better ssl keys and working https server on 3000

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+// Native
+const FS = require('fs');
+const PAHT = require('path');
+const HTTPS = require('https');
+
+// Libraries
 const EXPRESS = require('express');
 const V_HOST = require('vhost');
+
+// ....
+// Server.js
+// ....
+
+const SSL_KEY = FS.readFileSync('./_certs/localhost.key');
+const SSL_CERT = FS.readFileSync('./_certs/localhost.crt');
 
 const APP = EXPRESS();
 
@@ -9,4 +22,9 @@ const VALIDATOR = require('./apps/validator/validator-server.js');
 APP.use(V_HOST('wallet.localhost', WALLET));
 APP.use(V_HOST('validator.localhost', VALIDATOR));
 
-APP.listen(3000);
+const HTTP_SERVER = HTTPS.createServer({
+    key: SSL_KEY,
+    cert: SSL_CERT
+}, APP);
+
+HTTP_SERVER.listen(3000);
